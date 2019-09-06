2c0e3275bcd25b0da25c86009b7ad7eeeaa0d8cf
Marcello Angelini <marcello.angelini@ablabo.com>
2019-09-06 14:37:00 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const React =  require("react");
+const React = require("react");
 const ReactPDF = require("@react-pdf/renderer");
 
-const { Document , Text, Page } = ReactPDF; 
+const { Document, Text, Page } = ReactPDF;
 
 function hello() {
-	React.useEffect(() => {
-		console.log('wewe');
-	}, []);
-	
-	return React.createElement(Document, null, React.createElement(Page, null, React.createElement(Text, null, "hdllo ")));
+  React.useEffect(() => {
+    console.log("wewe");
+  }, []);
+
+  return React.createElement(
+    Document,
+    null,
+    React.createElement(Page, null, React.createElement(Text, null, "hdllo "))
+  );
 }
 
 ReactPDF.render(hello(), `${__dirname}/testing.pdf`);
